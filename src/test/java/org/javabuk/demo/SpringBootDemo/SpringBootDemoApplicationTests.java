1a9ef56fe6b43389bd15968a786404b35718e477
javabuk <joprieto@gmail.com>
2020-05-18 20:52:24 +0200

    Subida inicial

---
+++ src/test/java/org/javabuk/demo/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javabuk.demo.SpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
